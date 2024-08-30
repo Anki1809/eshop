749e80be50cfa85b1e04f483e27833f4ef313a79
Anki1809 <jobsforankitgupta@gmail.com>
2024-08-30 20:53:01 +0530

    product-service -> changes Added v3, shop-service -> v1 eureka added , eureka-server-service -> v1, gateway-service -> v1

---
+++ gateway-service/src/main/java/com/eshop/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,29 @@
+package com.eshop.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator eshopRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route(p -> p
+                        .path("/shop/**")
+                        .filters( f -> f.rewritePath("/shop/(?<segment>.*)","/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                        .uri("lb://SHOP-SERVICE")).build();
+
+
+    }
+}
